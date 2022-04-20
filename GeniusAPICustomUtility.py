2983f3e28150be51fc6d9721477a4a1afd8872ed
Erdem Dursunkaya <dreemrud@gmail.com>
2022-04-20 16:22:14 +0900

    Added GeniusAPICustomUtility.py

--- GeniusAPICustomUtility.py
+++ GeniusAPICustomUtility.py
@@ -1,2 +1,13 @@
+import config
+import re
 
-def parse_search_artists():
+# A library that provides a Python interface to the Genius API
+import lyricsgenius
+
+geniusAPI = lyricsgenius.Genius(config.GENIUS_API_TOKEN)
+
+def get_artist_names(name):
+    hitsList = geniusAPI.search_artists(name, per_page=3)['sections'][0]['hits']
+
+    return hitsList
+
--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import config
 import dummyinput
 
+import GeniusAPICustomUtility as GeniusUtil
+
 # I/O
 import os
 
 # Regex
 import re
 
-# A library that provides a Python interface to the Genius API
-import lyricsgenius
-
-
-genius = lyricsgenius.Genius(config.GENIUS_API_TOKEN)
-
-artists = genius.search_artists(dummyinput.ARTIST_NAME)
-
-
+artistsName = GeniusUtil.get_artist_names(dummyinput.ARTIST_NAME)
 
 print('End')
