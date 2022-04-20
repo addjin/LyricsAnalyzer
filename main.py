22c24bd8ee43293030bba37debc88385fc913af6
Erdem Dursunkaya <dreemrud@gmail.com>
2022-04-20 14:45:06 +0900

    Start project

---
+++ main.py
@@ -0,0 +1,14 @@
+GENIUS_API_TOKEN= 'ADD TOKEN'
+
+# Make HTTP requests
+import requests
+
+# Scrape data from HTML document
+from bs4 import BeautifulSoup
+
+# I/O
+import os
+
+# Regex
+import re
+
