f7ec01f1bbb2e153b0df6d2b18851e7b7459fe59
Erdem Dursunkaya <dreemrud@gmail.com>
2022-04-29 17:02:09 +0900

    mixed reset

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
+from LyricsAnalysisUtility import LyricsData
 import dummyinput
 import GeniusAPICustomUtility as GeniusUtil
 
 artist_Name = dummyinput.ARTIST_NAME
 
-artistNameArtistMap = GeniusUtil.get_artistName_artist_map(artist_Name)
+# artistNameArtistMap = GeniusUtil.get_artistName_artist_map(artist_Name)
 
-if len(artistNameArtistMap) == 0:
-    print('No results for artist \'{0}\'.'.format(artist_Name))
-    exit()
-else:
-    print('Candidates:')
+# if len(artistNameArtistMap) == 0:
+#     print('No results for artist \'{0}\'.'.format(artist_Name))
+#     exit()
+# else:
+#     print('Candidates:')
 
-    enumeratedArtistMap = {}
+#     enumeratedArtistMap = {}
 
-    for i, (key, val) in enumerate(artistNameArtistMap.items()):
-        enumeratedArtistMap[str(i)] = val
-        print('\t{0}: {1}'.format(i, key))
+#     for i, (key, val) in enumerate(artistNameArtistMap.items()):
+#         enumeratedArtistMap[str(i)] = val
+#         print('\t{0}: {1}'.format(i, key))
 
-inp = input('Input the number of the artist (or \'x\' to quit):')
-while inp not in enumeratedArtistMap.keys() and inp != 'x':
-    inp = input('Invalid input; please try again')
+# inp = input('Input the number of the artist (or \'x\' to quit):')
+# while inp not in enumeratedArtistMap.keys() and inp != 'x':
+#     inp = input('Invalid input; please try again')
 
-if inp == 'x':
-    exit()
-else:
-    selected_artist = enumeratedArtistMap[inp]
+# if inp == 'x':
+#     exit()
+# else:
+#     selected_artist = enumeratedArtistMap[inp]
 
 
-all_albums_list = GeniusUtil.get_all_albums(selected_artist)
+# all_albums_list = GeniusUtil.get_all_albums(selected_artist)
+# track_lyrics_map = GeniusUtil.get_lyrics_from_album(all_albums_list[0])
 
-track_lyrics_map = GeniusUtil.get_lyrics_from_album(all_albums_list[0])
+my_lyrics_data = None
+
+with open('squarehammer.txt', 'r') as samplelyrics:
+    my_lyrics_data = LyricsData(samplelyrics.read())
+
+print(my_lyrics_data._remove_notes())
 
 print('End')
+
+
